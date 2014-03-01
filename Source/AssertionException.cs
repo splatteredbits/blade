c99de92a8a866a77fa0596c84e11fdf41aecf1ad
Aaron Jensen <splatteredbits@users.noreply.github.com>
2014-02-28 18:25:49 -0800

    Blade now returns TestResult and RunResult objects. Blade tests are now directly runnable by blade. Improved failure/error reporting.

---
+++ Source/AssertionException.cs
@@ -0,0 +1,18 @@
+﻿namespace Blade
+{
+    public sealed class AssertionException : System.Exception
+    {
+        private readonly string[] _psStackTrace;
+
+        public AssertionException(string message, string[] psStackTrace)
+            : base(message)
+        {
+            _psStackTrace = psStackTrace;
+        }
+
+        public string[] PSStackTrace
+        {
+            get { return _psStackTrace; }
+        }
+    }
+}
---
+++ Source/TestResult.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Management.Automation;
+
+namespace Blade
+{
+    public sealed class TestResult
+    {
+        public TestResult(string fixtureName, string name)
+        {
+            FixtureName = fixtureName;
+            Name = name;
+            Passed = false;
+            Failure = null;
+            Error = null;
+            StartedAt = DateTime.Now;
+            Output = new List<object>();
+        }
+
+        public string FixtureName { get; private set; }
+        public string Name { get; private set; }
+        public bool Passed { get; private set; }
+        public AssertionException Failure { get; private set; }
+        public ErrorRecord Error { get; private set; }
+        public TimeSpan Duration { get; private set; }
+        public DateTime StartedAt { get; private set; }
+        public IList<object> Output { get; private set; }
+
+        public void Completed()
+        {
+            Passed = true;
+            Duration = DateTime.Now - StartedAt;
+        }
+
+        public void Completed(AssertionException ex)
+        {
+            Completed();
+            Passed = false;
+            Failure = ex;
+        }
+
+        public void Completed(ErrorRecord error)
+        {
+            Completed();
+            Passed = false;
+            Error = error;
+        }
+    }
+}

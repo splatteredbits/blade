a80a9a693e55a06aa6c79998ba578445b21cbcb3
Aaron Jensen <aaron@splatteredbits.com>
2014-03-02 09:22:33 -0800

    Finishing documentation.

--- Blade/Assert-ContainsLike.ps1
+++ Blade/Assert-ContainsLike.ps1
@@ -1,12 +1,49 @@
 
-function Assert-ContainsLike($haystack, $needle, $message)
+function Assert-ContainsLike
 {
-    foreach( $line in $haystack )
+    <#
+    .SYNOPSIS
+    Asserts that a collections contains an item, using wildcards to check.
+
+    .DESCRIPTION
+    Compares each item in a collection for a value using PowerShell's `-like` operator.
+
+    .EXAMPLE
+    Assert-ContainsLike @( 'foo', 'bar', 'baz' ) 'b*'
+
+    Demonstrates how to check if a collection contains an item, using wildcards when doing the comparison.
+
+    .EXAMPLE
+    Assert-ContainsLike @( 'foo', 'bar' ) '*z' 'No Z!'
+
+    Demonstrates how to supply your own message when the assertion fails.
+    #>
+    [CmdletBinding()]
+    param(
+        [Parameter(Position=0)]
+        [object]
+        # The collection to check.
+        $Haystack, 
+
+        [Parameter(Position=1)]
+        [object]
+        # The object to check the collection for. Wildcards supported.
+        $Needle, 
+
+        [Parameter(Position=2)]
+        [string]
+        # A message to show when the assertion fails.
+        $Message
+    )
+
+    Set-StrictMode -Version 'Latest'
+
+    foreach( $item in $Haystack )
     {
-        if( $line -like "*$needle*" )
+        if( $item -like "*$Needle*" )
         {
             return
         }
     }
-    Fail "Unable to find '$needle': $message" 
+    Fail "Unable to find '$Needle'. $Message" 
 }
--- Blade/Assert-DoesNotContain.ps1
+++ Blade/Assert-DoesNotContain.ps1
@@ -1,8 +1,45 @@
 
-function Assert-DoesNotContain($Haystack, $Needle, $Message)
+function Assert-DoesNotContain
 {
+    <#
+    .SYNOPSIS
+    Asserts that a collection doesn't contain an object/item.
+
+    .DESCRIPTION
+    Use's PowerShell's `-contains` operator to check that a collection is missing an object/item.
+
+    .EXAMPLE
+    Assert-DoesNotContains @( 1, 2, 3 ) 4
+
+    Demonstrates how to assert a collection doesn't contain an item.
+
+    .EXAMPLE
+    Assert-DoesNotContain @( 1, 2, 3 ) 3 'Three is the loneliest number.'
+
+    Demonstrates how to show your own message if the assertion fails.
+    #>
+    [CmdletBinding()]
+    param(
+        [Parameter(Position=0)]
+        [object]
+        # The collection to check.
+        $Haystack, 
+
+        [Parameter(Position=1)]
+        [object]
+        # The object the collection shouldn't have.
+        $Needle, 
+
+        [Parameter(Position=2)]
+        [string]
+        # A message to show when the assertion fails.
+        $Message
+    )
+
+    Set-StrictMode -Version 'Latest'
+
     if( $Haystack -contains $Needle )
     {
-        Fail "Found '$Needle': $Message"
+        Fail "Found '$Needle'. $Message"
     }
 }

bea87ce7a8d327ee8f4dcc85627cee4b6833f607
Aaron Jensen <aaron@splatteredbits.com>
2014-03-01 17:19:27 -0800

    Adding Blade documentation.

--- Blade/Assert-NodeDoesNotExist.ps1
+++ Blade/Assert-NodeDoesNotExist.ps1
@@ -1,6 +1,34 @@
 
-function Assert-NodeDoesNotExist($xml, $xpath, $defaultNamespacePrefix, $message)
+function Assert-NodeDoesNotExist
 {
+    <#
+    .SYNOPSIS
+    Asserts that a node doesn't exist in an XML document.
+
+    .DESCRIPTION
+    It's usually pretty easy in PowerShell to check if a node doesn't exist in an XML document: simply test if a property exists on that node.  If, however, that XML document has namespaces, you have to do extra setup with the XML document so that you can find the node.  This function does that work for you.
+
+    .LINK
+    Assert-NodeExists
+
+    .LINK
+    Test-NodeExists
+
+    .EXAMPLE
+    Assert-NodeDoesNotExist '<foo><bar><baz /></bar></foo>' '/foo/bar/biz'
+
+    Demonstrates how to assert that an XML document doesn't contain a node.
+    #>
+    [CmdletBinding()]
+    param(
+        $Xml, 
+        $XPath, 
+        $DefaultNamespacePrefix, 
+        $Message
+    )
+
+    Set-StrictMode -Version 'Latest'
+
     if( Test-NodeExists $xml $xpath $defaultNamespacePrefix )
     {
         Fail "Found node with xpath '$xpath': $message"

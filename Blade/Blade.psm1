29641f1da98706da428ab2a7e1422015d04c1ad6
Aaron Jensen <splatteredbits@users.noreply.github.com>
2014-03-02 10:07:23 -0800

    Excluding internal functions from export.

--- Blade/Blade.psm1
+++ Blade/Blade.psm1
@@ -7,4 +7,15 @@ Get-Item -Path (Join-Path $PSScriptRoot '*-*.ps1') |
     Where-Object { -not $doNotImport.ContainsKey( $_.BaseName ) } |
     ForEach-Object { . $_.FullName }
 
-Export-ModuleMember -Function * -Alias *
+$privateFunctions = @{ 
+                        'Remove-ItemWithRetry' = $true; 
+                        'Set-CurrentTest' = $true; 
+                        'Set-TestVerbosity' = $true; 
+                    }
+
+$publicFunctions = Get-ChildItem -Path 'function:\' |
+                        Where-Object { $_.ModuleName -eq 'Blade' } |
+                        Where-Object { -not $privateFunctions.ContainsKey( $_.Name ) } |
+                        Select-Object -ExpandProperty 'Name'
+
+Export-ModuleMember -Function $publicFunctions -Alias *
